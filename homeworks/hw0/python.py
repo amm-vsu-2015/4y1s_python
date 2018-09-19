12518f6d29819f5da54f6c949c52bbb0a50eaea4
pavbox <pavboxdev@gmail.com>
2018-09-20 00:45:36 +0300

    uploaded base test code as hw0

---
+++ homeworks/hw0/python.py
@@ -0,0 +1,15 @@
+
+# Python REPL
+# You can execute program with next commands: (in current folder)
+# python python.py
+
+# declare function
+def printArray(array):
+    for x in a:
+        print(x)
+
+# setup array
+a = [1,"лол",4.]
+
+# call function with parameter
+printArray(a)
